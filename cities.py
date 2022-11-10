ebbcef94eafcf1625477735162f00fc09874fb6e
Shameer <shameer.ahmad.19@ucl.ac.uk>
2022-11-10 14:22:38 +0000

    Defined `City` propeties/variables and created tests for them.

--- cities.py
+++ cities.py
@@ -1,7 +1,23 @@
 from typing import Dict, List, Tuple
 
 class City:
-    ...
+    def __init__(self, city, country, attendee_num, latitude, longitude):
+        self.city = city
+        self.country = country
+        self.attendee_num = attendee_num
+        self.latitude = latitude
+        self.longitude = longitude
+
+        if attendee_num < 0:
+            raise ValueError("Number of attendees is negative") 
+
+        if latitude > 90 or latitude < -90:
+            raise ValueError("Latitude is invald; range must be (-90, 90)") 
+            
+        if longitude >180 or longitude <-180:
+            raise ValueError("Longitude is invald; range must be (-180, 180)") 
+
+    
     def distance_to(self, other: 'City') -> float:
         raise NotImplementedError
 
