0978b2d6ea59f7829fff3883d0c90e894d8c4d17
Shameer <shameer.ahmad.19@ucl.ac.uk>
2022-11-10 14:37:54 +0000

    Completed distance_to() function to calculate the distance to a specific city using Haversine formula. Also, Added more tests.

--- cities.py
+++ cities.py
@@ -1,4 +1,5 @@
 from typing import Dict, List, Tuple
+from math import asin, sin, cos, sqrt
 
 class City:
     def __init__(self, city, country, attendee_num, latitude, longitude):
@@ -8,18 +9,43 @@ class City:
         self.latitude = latitude
         self.longitude = longitude
 
+
+
+        #create errors to  check for valud values and types of properties:
         if attendee_num < 0:
             raise ValueError("Number of attendees is negative") 
+        if type(attendee_num) != int:
+            raise ValueError("Number of attendees must be an integer")
+
 
         if latitude > 90 or latitude < -90:
-            raise ValueError("Latitude is invald; range must be (-90, 90)") 
-            
+            raise ValueError("Latitude is invald; must be between (-90, 90)") 
         if longitude >180 or longitude <-180:
-            raise ValueError("Longitude is invald; range must be (-180, 180)") 
+            raise ValueError("Longitude is invald; must be between (-180, 180)") 
+
+
+        if type(longitude) != float or type(longitude) != int:
+            raise ValueError("Longitude must be an integer or float") 
+        if type(latitude) != float or type(latitude) != int:
+            raise ValueError("Latitude must be an integer or float") 
+
+
+
+
 
     
     def distance_to(self, other: 'City') -> float:
-        raise NotImplementedError
+        '''use Haversine formula to calculate distance between 2 cities'''
+
+        R=6371 #radius of earth
+        first_term = sin(other.latitude/2  -  self.latitude/2)**2 #first term in equation
+        second_term = cos(self.latitude)*cos(other.latitude)*( sin(other.longitude - self.longitude)**2 ) #second term in equation
+
+        d = 2*R*asin(sqrt(  first_term + second_term   )) #distance 
+
+        return d
+    
+
 
     def co2_to(self, other: 'City') -> float:
         raise NotImplementedError
