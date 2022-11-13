63b294b7020a3086e6f105ddc577c71e325bcebf
Shameer <shameer.ahmad.19@ucl.ac.uk>
2022-11-13 17:33:08 +0000

    Completed read_attendees_file() function to create a `CityCollection` object from a csv file containing properties of different cities.

--- utils.py
+++ utils.py
@@ -1,4 +1,55 @@
 from cities import City, CityCollection
+from pathlib import Path
+import csv
+
+file_path = Path("attendee_locations.csv")
 
 def read_attendees_file(filepath: Path) -> CityCollection:
-    raise NotImplementedError
+    with open(filepath) as file:
+        csv_reader = csv.reader(file, delimiter=',')
+
+        next(csv_reader) #skip header
+
+        #attendee_nums = [int(row[0]) for row in csv_reader]
+        #countries = [str(row[1]) for row in csv_reader]
+        #US_states =  [str(row[2]) for row in csv_reader]
+        #cities =  [str(row[3]) for row in csv_reader]
+        #latitudes =  [float(row[4]) for row in csv_reader]
+        #longitudes =  [float(row[5]) for row in csv_reader]
+
+        attendee_nums =[]
+        countries=[]
+        US_states=[]
+        cities=[]
+        latitudes=[]
+        longitudes=[]
+
+
+        for row in csv_reader:
+            attendee_nums.append( int(row[0]) )
+            countries.append( str(row[1]) )
+            US_states.append( str(row[2]) )
+            cities.append( str(row[3]) )
+            latitudes.append( float(row[4]) )
+            longitudes.append( float(row[5]) )
+
+
+        all_City_objects = [City(cities[i], countries[i], attendee_nums[i], latitudes[i], longitudes[i]) for i in range(len(cities))]
+
+        all_City_objects_dict = {}
+         
+        for i in range(len(cities)):
+            all_City_objects_dict[cities[i]] = all_City_objects[i]
+
+        #print(attendee_nums)
+        #print(countries)
+
+        #for i in range(len(cities)):
+            #exec("%s = %d" % (cities[i], all_City_objects[i]))
+
+        return all_City_objects_dict
+
+
+collection = read_attendees_file(file_path)
+
+print(collection)
